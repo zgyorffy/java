79193750da96822d98b343e545e447c2177b3dc4
Zsolt Gyorffy <zsoltgyorffy@gmail.com>
2019-04-08 11:56:23 +0300

    properties

--- props/src/com/props/mypackage/Main.java
+++ props/src/com/props/mypackage/Main.java
@@ -3,6 +3,8 @@ package com.props.mypackage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
@@ -24,6 +26,20 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println(e.getClass().getSimpleName() +  " - " + e.getMessage());
 		}
+		
+		//property XML file creation
+		try {
+			propsXMLWriter();
+		} catch (Exception e) {
+			System.out.println(e.getClass().getSimpleName() +  " - " + e.getMessage());
+		}
+		
+		//property XML file reader
+		try {
+			propsXMLReader();
+		} catch (Exception e) {
+			System.out.println(e.getClass().getSimpleName() +  " - " + e.getMessage());
+		}
 
 	}
 	
@@ -39,6 +55,27 @@ public class Main {
 		System.out.println(val2);
 	}
 	
+	public static void propsXMLReader() throws IOException {
+		Properties props = new Properties();
+		
+		try (InputStream in = Files.newInputStream(Paths.get("props.xml"))) {
+			props.loadFromXML(in);
+		}
+		String val1 = props.getProperty("name");
+		String val2 = props.getProperty("mobile");
+		System.out.println(val1);
+		System.out.println(val2);
+	}
+	
+	public static void propsXMLWriter() throws IOException {
+		Properties props = new Properties();
+		props.setProperty("name", "Zsolt");
+		props.setProperty("mobile", "0744xxx");
+		
+		try (OutputStream out = Files.newOutputStream(Paths.get("props.xml"))) {
+			props.storeToXML(out, "my comment");
+		}
+	}
 	public static void propsWriter() throws IOException {
 		Properties props = new Properties();
 		props.setProperty("name", "Zsolt");
